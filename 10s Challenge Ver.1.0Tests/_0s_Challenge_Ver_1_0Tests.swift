ed889036a5d2c0f66196788fa8c0bddf9d451a76
田中涼誠 <iesoyr907@icloud.com>
2018-08-07 14:23:39 +0900

    Initial Commit

---
+++ 10s Challenge Ver.1.0Tests/_0s_Challenge_Ver_1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0s_Challenge_Ver_1_0Tests.swift
+//  10s Challenge Ver.1.0Tests
+//
+//  Created by Ryosei Tanaka on 2018/08/07.
+//  Copyright © 2018年 Ryosei Tanaka. All rights reserved.
+//
+
+import XCTest
+@testable import _0s_Challenge_Ver_1_0
+
+class _0s_Challenge_Ver_1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
